ac52f3f34b00ae17bca5482d94b75dacaf2e173b
vk-it <46228289+vk-it@users.noreply.github.com>
2020-03-27 20:52:07 +0300

    Сделал дз 1

--- js/script.js
+++ js/script.js
@@ -1,24 +1,45 @@
-var budget = prompt('Сколько Вы зарабатываете?', '50.000 рублей');
+// var budget = prompt('Сколько Вы зарабатываете?', '50.000 рублей');
 
-alert(budget + ' хорошая сумма!')
+// alert(budget + ' хорошая сумма!')
 
-var nameStore = prompt('Введите название Вашего магазина', 'Успех');
+// var nameStore = prompt('Введите название Вашего магазина', 'Успех');
 
-alert(nameStore + ' интересное название!')
+// alert(nameStore + ' интересное название!')
 
-const mainList = {
-    budget: 1000000,
-    nameStore: 'Успех',
-    shopGoods: ['vest', 'shorts', 'trainers'],
-    employers: { // почему объект?
-      john: {
-        name: 'John',
-        age: 23
-      },
-      rast: {
-        name: 'Rast',
-        age: 28
-      }
-    },
-    open: true // видимо, открыт магазин или нет
-  }
+// const mainList = {
+//     budget: 1000000,
+//     nameStore: 'Успех',
+//     shopGoods: ['vest', 'shorts', 'trainers'],
+//     employers: { // почему объект?
+//       john: {
+//         name: 'John',
+//         age: 23
+//       },
+//       rast: {
+//         name: 'Rast',
+//         age: 28
+//       }
+//     },
+//     open: true // видимо, открыт магазин или нет
+//   }
+
+// Задание 1
+
+let money = prompt('Ваш бюджет?');
+let name = prompt('Название вашего магазина?');
+
+let mainList = {
+    budget: money,
+    shopName: name,
+    shopGoods: [],
+    employers: {},
+    open: false
+}
+
+mainList.shopGoods[0] = prompt('Какой тип товаров будем продавать?');
+mainList.shopGoods[1] = prompt('Какой тип товаров будем продавать?');
+mainList.shopGoods[2] = prompt('Какой тип товаров будем продавать?');
+
+alert(mainList.budget / 30);
+
+console.log(mainList)
