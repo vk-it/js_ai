4a6819579db8e1827359e6455226764ed00e8bc7
vk-it <46228289+vk-it@users.noreply.github.com>
2020-04-10 21:34:28 +0300

    Начал делать задание 2

--- js/script.js
+++ js/script.js
@@ -58,3 +58,30 @@
 //     y = 7;
 //     x = y;
 //     console.log(x)
+
+let money = prompt('Ваш бюджет?');
+let name = prompt('Название вашего магазина?');
+
+let mainList = {
+    budget: money,
+    shopName: name,
+    shopGoods: [],
+    employers: {},
+    open: false
+}
+
+for (let i = 0; i < 5; i++) {
+
+    let a = prompt('Какой тип товаров будем продавать?');
+
+    if ((typeof(a)) === 'string' && (typeof(a)) === null && a != '' && a.length < 50 ) {
+        console.log('Всё верно!');
+        mainList.shopGoods[i] = a;
+    } else {
+        i = i - 1;
+    }
+}
+
+alert(mainList.budget / 30);
+
+console.log(mainList)
